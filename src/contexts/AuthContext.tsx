cf930ea2c63549d46cac17d03f099c0366cff983
FelipeDaros <felipe-daros@hotmail.com>
2023-07-08 00:36:47 -0300

    Alterações

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,5 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { UserType } from "./UserType";
 import { api } from "../config/api";
 
 type AuthContextDataProps = {
